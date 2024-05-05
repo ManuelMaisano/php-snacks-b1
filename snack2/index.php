8d3570831bc947f8d778910166f3c82312f06b2a
Manuel Maisano <manumaisano@gmail.com>
2024-05-05 22:29:44 +0200

    snack2

---
+++ snack2/index.php
@@ -0,0 +1,49 @@
+
+<?php 
+
+//Snack 2
+//Con un form passare come parametri GET name, mail e age e verificare 
+//(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+//che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/script.js" type="module"></script>
+    <title>Snack2</title>
+
+</head>
+
+
+<body>
+  
+
+
+</body>
+</html>
