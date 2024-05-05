32b84da07f4eada8f7814f6b27c030e4dd9019d1
Manuel Maisano <manumaisano@gmail.com>
2024-05-05 22:34:00 +0200

    snack3

---
+++ snack3/index.php
@@ -0,0 +1,52 @@
+
+<?php 
+
+//Snack 3
+//Creare un array di array. 
+//Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+//e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/script.js" type="module"></script>
+    <title>Snack3</title>
+
+</head>
+
+
+<body>
+  
+
+</body>
+</html>
