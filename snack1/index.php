8a4ea47abb77632c91155f7ac5d7f0d98dab6ce0
Manuel Maisano <manumaisano@gmail.com>
2024-05-05 22:19:26 +0200

    snack1 consegna

---
+++ snack1/index.php
@@ -0,0 +1,34 @@
+<?php
+
+//Snack 1
+//Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+//Olimpia Milano - Cantù | 55-60
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/script.js" type="module"></script>
+    <title>Snack1</title>
+
+</head>
+
+
+<body>
+    
+
+
+
+</body>
+</html>
